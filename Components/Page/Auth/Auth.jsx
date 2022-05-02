61c196fdd0c7a360c9bc335033d53fbbecd59614
NewEra <sam.djm93@gmail.com>
2022-05-02 15:26:33 +0200

    SignUpForm et LoginForm

--- Components/Page/Auth/Auth.jsx
+++ Components/Page/Auth/Auth.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import LoginForm from "../../Container/LoginForm/LoginForm";
+import SignUpForm from "../../Container/SignUpForm/SignUpForm";
 import Card from "../../HOC/Card/Card";
 
 export default function Auth() {
+  //1- Créer la variable d'état:
+  const [isLogin, setIsLogin] = useState(true);
+
+  //2- Créer la fonction pour modifier la variable:
+  function toggleIsLogin() {
+    setIsLogin(!isLogin);
+  }
+
+  //3- Utiliser l'opérateur ternaire pour afficher conditionnelement les composant: LoginForm et SignUpForm
+
   return (
     <View style={styles.container}>
       <Card title='Bienvenu!' content='Veuillez vous authentifier.'>
-        <Text>Un composant en children</Text>
+        {isLogin ? <LoginForm /> : <SignUpForm />}
+        <Text onPress={toggleIsLogin}>
+          {isLogin
+            ? "Pas encore membre? Inscrivez-vous!"
+            : "Vous etes deja membre? Connectez-vous!"}
+        </Text>
       </Card>
     </View>
   );
