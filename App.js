5b4df7de10d90acb6696bcc5aded012bed0a61cc
NewEra <sam.djm93@gmail.com>
2022-05-04 10:51:33 +0200

    Exercice Informations utilisateur

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgb(150, 150, 150)",
+    backgroundColor: "rgb(250, 250, 250)",
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
--- Components/Page/Profil/Profil.jsx
+++ Components/Page/Profil/Profil.jsx
@@ -1,17 +1,56 @@
 // rnfc : React Native Functional Component
 
+import { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { UserContext } from "../../../Contexts/UserContext";
+import { styleVariables } from "../../../variables/StyleVariables";
 
 const Profil = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <View style={styles.container}>
-      <Text>Sam</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoLabel}>Email</Text>
+        <Text style={styles.info}> {user.email} </Text>
+      </View>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoLabel}>Username</Text>
+        <Text style={styles.info}> {user.username} </Text>
+      </View>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoLabel}>Description</Text>
+        <Text style={styles.info}>
+          {user.description
+            ? user.description
+            : "Veuillez entrer une description..."}
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "rgb(220, 220, 220)",
+    borderBottomWidth: 2,
+    borderTopWidth: 2,
+    borderColor: styleVariables.primaryColor,
+    padding: 20,
+  },
+  infoContainer: {
+    borderBottomWidth: 2,
+    borderColor: "gray",
+    paddingBottom: 10,
+  },
+  info: {
+    fontSize: 15,
+  },
+  infoLabel: {
+    fontWeight: "bold",
+  },
 });
 
 export default Profil;
