817cc0da89c6f307d844bd03c2b3eef4be1255a9
NewEra <sam.djm93@gmail.com>
2022-05-05 10:55:22 +0200

    Ajout de takePicture

--- Components/Page/Cam/Cam.jsx
+++ Components/Page/Cam/Cam.jsx
@@ -1,6 +1,12 @@
 //import liraries
 import { Camera } from "expo-camera";
-import React, { Component, useEffect, useState } from "react";
+import React, {
+  Component,
+  useEffect,
+  useState,
+  useRef,
+  useContext,
+} from "react";
 import {
   View,
   Text,
@@ -12,10 +18,35 @@ import {
 import { styleVariables } from "../../../variables/StyleVariables";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
+import { UserContext } from "../../../Contexts/UserContext";
 // create a component
-const Cam = () => {
+const Cam = (props) => {
   const size = useWindowDimensions();
   const [cameraPermission, setCameraPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const camRef = useRef();
+
+  const { user, setUser } = useContext(UserContext);
+
+  //Créer une fonction qui fait passer la variable cameraType de front a back ou de back a front:
+  function toggleCameraType() {
+    setCameraType(
+      cameraType === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  }
+
+  async function takePicture() {
+    let photo = await camRef.current.takePictureAsync();
+    console.log("photo", photo);
+    //Utiliser le contexte pour modifier l'avatar de user avec la photo.
+    if (photo) {
+      setUser({ ...user, avatar: photo });
+      //Utiliser la navigation pour revenir sur la page de profil.
+      props.navigation.goBack();
+    }
+  }
 
   useEffect(() => {
     (async () => {
@@ -52,22 +83,24 @@ const Cam = () => {
   return (
     <View style={styles.container}>
       <Camera
+        ref={camRef}
+        type={cameraType}
         ratio='16:9'
         style={{ width: size.width, height: (size.width * 16) / 9 }}
       >
         <View style={styles.iconsContainer}>
-          <TouchableOpacity>
-            <MaterialIcons
-              name='camera'
-              size={64}
-              color={styleVariables.danger}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={toggleCameraType}>
             <MaterialIcons
               name='flip-camera-ios'
               size={64}
               color={styleVariables.success}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={takePicture}>
+            <MaterialIcons
+              name='camera'
+              size={64}
+              color={styleVariables.danger}
             />
           </TouchableOpacity>
         </View>
